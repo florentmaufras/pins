a090dfc49dd0f41bba4e19e706da1d7fec71d9df
Florent Maufras <florent.maufras@carebook.com>
2023-08-08 22:50:41 -0400

    Normalize padding size

---
+++ app/src/main/java/com/demo/pins/ui/common/Padding.kt
@@ -0,0 +1,12 @@
+package com.demo.pins.ui.common
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Padding(val value: Dp) {
+    EXTRA_SMALL(2.dp),
+    SMALL(4.dp),
+    DEFAULT(8.dp),
+    LARGE(12.dp),
+    EXTRA_LARGE(16.dp)
+}
